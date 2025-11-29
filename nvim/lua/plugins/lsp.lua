834fa5dbade31b544b04fd104a188d2b2b83851b
AndreyS89 <samarinandrey55@gmail.com>
2025-11-29 16:09:35 +0000

    Fix: Update nvim plugin lockfile from working server

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -9,23 +9,16 @@ return {
     "L3MON4D3/LuaSnip",
   },
   config = function()
-    -- 1. Запускаем Mason
     require("mason").setup()
-
-    -- 2. Настраиваем связку Mason и LSP
     require("mason-lspconfig").setup({
-      -- Список серверов для автоматической установки
       ensure_installed = { "ts_ls", "html", "cssls", "lua_ls", "eslint", "emmet_language_server" },
-
       handlers = {
-        -- Стандартная настройка для большинства серверов
         function(server_name)
           local capabilities = require('cmp_nvim_lsp').default_capabilities()
           require("lspconfig")[server_name].setup({
             capabilities = capabilities,
             on_attach = function(_, bufnr)
-              local opts = { buffer = bufnr, noremap = true, silent = true }
-              -- Горячие клавиши LSP
+              local opts = { buffer = bufnr, silent = true }
               vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
               vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
               vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
@@ -34,29 +27,19 @@ return {
             end,
           })
         end,
-
-        -- Специфичная настройка для Emmet
         ["emmet_language_server"] = function()
           local capabilities = require('cmp_nvim_lsp').default_capabilities()
           capabilities.textDocument.completion.completionItem.snippetSupport = true
-
           require("lspconfig").emmet_language_server.setup({
             capabilities = capabilities,
-            filetypes = {
-              "css", "eruby", "html", "javascript", "javascriptreact",
-              "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue"
-            },
+            filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue" },
           })
         end,
-
-        -- Специфичная настройка для Lua
         ["lua_ls"] = function()
           local capabilities = require('cmp_nvim_lsp').default_capabilities()
           require("lspconfig").lua_ls.setup({
             capabilities = capabilities,
-            settings = {
-              Lua = { diagnostics = { globals = { "vim" } } }
-            }
+            settings = { Lua = { diagnostics = { globals = { "vim" } } } }
           })
         end,
       }
